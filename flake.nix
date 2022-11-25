2e51532cc000e1d5d03a4433861216c590e51604
Emory Merryman <emory.merryman@gmail.com>
2022-11-25 10:23:06 -0500

    TESTED

--- flake.nix
+++ flake.nix
@@ -36,8 +36,9 @@
 				        filtered = builtins.filter ( l : builtins.typeOf l == "lambda" ) [ value undefined identity ] ;
 					in if builtins.length filtered == 0 then builtins.throw "020ecb5f-9b6d-4e0a-ad14-5005be7b29f5" else builtins.elemAt filtered 0 ;
 			          identity = x : x ;
-			          is-list = type == "list" ;
-			          is-simple = builtins.any ( t : t == type ) [ "bool" "float" "int" "lambda" "null" "path" "string" ] ;
+				  initial =
+				    if is-simple then value
+				    else null ;
 				  input =
 				    {
 				      bool = bool ;
@@ -48,14 +49,25 @@
 				      path = path ;
 				      string = string ;
 				    } ;
+			          is-list = type == "list" ;
+			          is-simple = builtins.any ( t : t == type ) [ "bool" "float" "int" "lambda" "null" "path" "string" ] ;
 			          lambda = if builtins.hasAttr type output then builtins.getAttr type output else builtins.throw "a0015af2-57e5-4a16-8e06-74408562c1bf" ;
+				  list =
+				    if is-simple then [ ]
+				    else null ;
 				  output = builtins.mapAttrs find input ;
-				  visit = track : if is-simple then lambda track else null ;
+				  reducer =
+				    if is-simple then previous : current : previous
+				    else null ;
+				  visit = track : builtins.foldl' reducer initial list ;
 				  in
 				    {
 				      find = find ;
 				      identity = identity ;
 				      input = input ;
+				      is-list = is-list ;
+				      is-simple = is-simple ;
+				      lambda = lambda ;
 				      output = output ;
 				      undefined = undefined ;
 				      visit = visit ;
