4277542b40bad234b8d3dc2ff99b19e9ccd30501
Emory Merryman <emory.merryman@gmail.com>
2022-12-23 20:05:24 -0500

    TESTED

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
                                 let
                                   size =
                                     input :
-                                      if builtins.typeOf input == "list" then builtins.trace "functions.size list" ( builtins.foldl' reducers.size 0 input )
+                                      if builtins.typeOf input == "list" then builtins.foldl' reducers.size 0 input
                                       else if builtins.typeOf input == "set" then builtins.foldl' reducers.size 0 ( builtins.attrValues input )
                                       else 1 ;
                                   in
