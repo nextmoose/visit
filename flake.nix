f40f39f68250cc3916029cb97a7d1f8691664426
Emory Merryman <emory.merryman@gmail.com>
2022-12-23 22:38:49 -0500

    TESTED

--- flake.nix
+++ flake.nix
@@ -31,9 +31,9 @@
                                 let
                                   size =
                                     input :
-                                      if builtins.typeOf input == "list" then let x = builtins.foldl' reducers.size 0 input ; in builtins.trace "LIST ${ builtins.toString x }" x
-                                      else if builtins.typeOf input == "set" then let x = builtins.foldl' reducers.size 0 ( builtins.attrValues input ) ; in builtins.trace "SET ${ builtins.toString x }" x
-                                      else builtins.trace "SIMPLE 1" 1 ;
+                                      if builtins.typeOf input == "list" then let x = builtins.foldl' reducers.size 0 input ; in builtins.trace "X LIST ${ builtins.toString x }" x
+                                      else if builtins.typeOf input == "set" then let x = builtins.foldl' reducers.size 0 ( builtins.attrValues input ) ; in builtins.trace "X SET ${ builtins.toString x }" x
+                                      else builtins.trace "X SIMPLE 1" 1 ;
                                   in
                                     {
                                       size = size ;
